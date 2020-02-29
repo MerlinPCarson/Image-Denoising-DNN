c97fbd9613b87a49fdb437843105de150e79917c
Merlin Carson <mpc6@.pdx.edu>
2020-02-28 20:53:13 -0800

    added init_weights function

--- DnCNN.py
+++ DnCNN.py
@@ -2,17 +2,6 @@ import torch.nn as nn
 import math
 
 
-def weights_init_kaiming(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        nn.init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
-    elif classname.find('Linear') != -1:
-        nn.init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
-    elif classname.find('BatchNorm') != -1:
-        # nn.init.uniform(m.weight.data, 1.0, 0.02)
-        m.weight.data.normal_(mean=0, std=math.sqrt(2./9./64.)).clamp_(-0.025,0.025)
-        nn.init.constant_(m.bias.data, 0.0)
-
 def init_weights(layer):
     if isinstance(layer, nn.Conv2d):
         nn.init.kaiming_normal_(layer.weight.data, a=0, mode='fan_in')
